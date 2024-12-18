fa60c421988279520445887ff83557218514e62b
Djouph <s218525517@ariel.ort.org.il>
2024-12-18 15:01:04 +0200

    new

---
+++ website/pages/login.ts
@@ -0,0 +1,6 @@
+import { send } from "../utilities";
+let passwordInput = document.getElementById("passwordInput") as HTMLInputElement;
+let usernameInput = document.getElementById("usernameInput") as HTMLInputElement;
+let loginButton = document.getElementById("loginButton") as HTMLInputElement;
+
+
