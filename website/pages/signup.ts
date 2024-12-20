189b9e32e5103c0d4747545cba697186fa04a9e0
liyaa <liyaaroeta2@gmail.com>
2024-12-20 11:22:52 +0200

    new

--- website/pages/signup.ts
+++ website/pages/signup.ts
@@ -3,10 +3,7 @@ let passwordInput = document.getElementById("passwordInput") as HTMLInputElement
 let usernameInput = document.getElementById("usernameInput") as HTMLInputElement;
 let signupButton = document.getElementById("signupButton") as HTMLInputElement;
 signupButton.onclick = async function(){
-        let [userfound, userId] = await send ("login", [usernameInput.value, passwordInput.value]) as [boolean, string]
-    send ("signup",[usernameInput.value, passwordInput.value]);
-    if(userfound){
+        let userId = await send ("signup", [usernameInput.value, passwordInput.value]) as  string;
     localStorage.setItem("userId" ,userId);
     location.href ="index.html";
-}
 }
