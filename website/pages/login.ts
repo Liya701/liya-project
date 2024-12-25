9833beb1a276de785796344306cf9d5b30318723
Djouph <s218525517@ariel.ort.org.il>
2024-12-25 10:12:10 +0200

    hey

--- website/pages/login.ts
+++ website/pages/login.ts
@@ -1,7 +1,7 @@
 import { send } from "../utilities";
 let passwordInput = document.getElementById("passwordInput") as HTMLInputElement;
 let usernameInput = document.getElementById("usernameInput") as HTMLInputElement;
-let loginButton = document.getElementById("loginButton") as HTMLInputElement;
+let loginButton = document.getElementById("loginButton") as HTMLButtonElement;
 loginButton.onclick = async function () {
     let [userfound, userId] = await send ("login", [usernameInput.value, passwordInput.value]) as [boolean, string]
     console.log("user found:" + userfound)
--- website/pages/signup.ts
+++ website/pages/signup.ts
@@ -1,7 +1,7 @@
 import { send } from "../utilities";
 let passwordInput = document.getElementById("passwordInput") as HTMLInputElement;
 let usernameInput = document.getElementById("usernameInput") as HTMLInputElement;
-let signupButton = document.getElementById("signupButton") as HTMLInputElement;
+let signupButton = document.getElementById("signupButton") as HTMLButtonElement;
 signupButton.onclick = async function(){
         let userId = await send ("signup", [usernameInput.value, passwordInput.value]) as  string;
     localStorage.setItem("userId" ,userId);
